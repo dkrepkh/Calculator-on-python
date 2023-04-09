fa3b05df84dd3d4a456e8c93b9455766880c09f1
dkrepkh <51156498+dkrepkh@users.noreply.github.com>
2023-04-09 05:10:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import re as regular_expression
+
+
+def calculator(expression):
+    try:
+        if regular_expression.search(r"0\s*\*\*\s*0", expression):
+            raise ZeroDivisionError("0 to the power of 0 undefined")
+        print(eval(expression))
+    except (SyntaxError, NameError, ZeroDivisionError, TypeError, ValueError) as e:
+        print(f"Error: {e}")
+
+
+def main():
+    while True:
+        print("if you want to leave write exit")
+        user_input = input("Enter a mathematical expression: ")
+        if user_input == "exit":
+            break
+        else:
+            calculator(user_input)
+
+
+if __name__ == '__main__':
+    main()
